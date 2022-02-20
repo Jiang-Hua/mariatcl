fd44bdba6e2d40ee6875b2c3d1f89cde5823ebcd
Steve Shaw <smshaw@users.sourceforge.net>
2022-02-20 17:28:58 +0000

    Update for Bawt build

--- generic/mariatcl.c
+++ generic/mariatcl.c
@@ -981,7 +981,7 @@ static int Mariatcl_Connect(ClientData clientData, Tcl_Interp *interp, int objc,
 #if (MYSQL_VERSION_ID >= 32350)
   if (mysql_options_reconnect)
   {
-    bool reconnect = 1;
+    _Bool reconnect = 1;
     mysql_options(handle->connection, MYSQL_OPT_RECONNECT, &reconnect);
   }
   mysql_options(handle->connection, MYSQL_READ_DEFAULT_GROUP, groupname);
